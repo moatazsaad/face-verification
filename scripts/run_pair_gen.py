42f815d5add698fb6f69faa4896c0eb2cb7d62a8
Ankan Roy <ankanroy@Ankans-MacBook-Pro-2.local>
2026-02-25 19:02:57 -0500

    Minor fix

--- scripts/run_pair_gen.py
+++ scripts/run_pair_gen.py
@@ -6,8 +6,8 @@ from src.data_ingest import ingest_lfw
 from src.pair_gen import save_splits, generate_pairs
 
 def main():
-    print("Pair generation and saving in progress...")
     labels, dataset_split = ingest_lfw()
+    print("Pair generation and saving in progress...")
     save_splits(labels, dataset_split)
 
 if __name__ == "__main__":
